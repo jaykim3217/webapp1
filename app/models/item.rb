02c3344811cbdfb9c0527b1b05db8d31191f45c4
jaykim3217 <102015699+jaykim3217@users.noreply.github.com>
2022-06-11 11:52:16 +0900

    error_messege カラム日本語化

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,7 +7,7 @@ class Item < ApplicationRecord
 
   has_one_attached :image
 
-  validates :category_id, presence: true, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :category_id, presence: true, numericality: { other_than: 1 , message: "を入力してください"}
   validates :image, presence: true
   validates :item_name, presence: true
   validates :explain, presence: true
